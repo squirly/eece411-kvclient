75ab88a656468b11ee999faab30a7a15cb9829fd
marilyne <marilyn74@gmail.com>
2014-04-04 16:38:11 -0700

    Add unit testng framework for clustered and throughput tests

--- tests/testing/test_mass_test.py
+++ tests/testing/test_mass_test.py
@@ -1,10 +1,36 @@
 from testing.mass_test import MassTestBase
+from kvclient.base import KeyValueClient
+from mock import MagicMock, Mock, patch
 
+
+class MockMassTest(MassTestBase):
+    def get_client(self):
+        return self.client
+
+    def reset_test(self):
+        pass
 
 class TestMassTestBase(object):
+
     def test_create(self):
-        test = MassTestBase(['1.2.3.4', '4.3.2.1'])
+        test = MockMassTest(['1.2.3.4', '4.3.2.1'])
+        test.client = MagicMock()
+        test.keys = range(0, 8)
+        test.value_one = 1
+        test.value_two = 2
+
         test. \
-            set_key_count(128). \
-            set_concurrency(16). \
-            set_timeout(0)
+            set_key_count(8). \
+            set_concurrency(2). \
+            set_timeout(1)
+
+        test.run_test()
+
+        calls = test.client.get.call_args_list
+        args_list = [arguments[0][0] for arguments in calls]
+
+        assert set(args_list) == set(test.keys)
+        assert test.client.get.call_count == 16
+        assert test.client.put.call_count == 16
+        assert test.client.delete.call_count == 8
+
